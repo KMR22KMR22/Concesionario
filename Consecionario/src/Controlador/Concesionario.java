97ac3eb0e7da06be3b66e378ba1ee6f64e9aa483
unknown <k.mendez4229@gmail.com>
2025-12-12 02:29:47 -0400

    agregue comentarios

--- Consecionario/src/Controlador/Concesionario.java
+++ Consecionario/src/Controlador/Concesionario.java
@@ -22,6 +22,9 @@ public class Concesionario {
     //Creo el ArrayList de los coches
     static List<Coche> coches = new ArrayList<>();
 
+    /**
+     * Llena el arraylist (coches) con coches predeterminados para que cuando arranque el progreama el concesionario ya cuente con coches
+     * */
     public void declaracionCoches() {
         coches.add(new Coche( "Lamborghini", "Gallardo", 2011, 100000, "1111ABC", false, 32000));
         coches.add(new Coche( "Lamborghini", "Aventador", 2021, 500000, "1111ABC", false, 0));
@@ -34,6 +37,9 @@ public class Concesionario {
     //Creo el ArrayList de los clientes
     public List<Cliente> clientes = new ArrayList<>();
 
+    /**
+     * Llena el arraylist (clientes) con clientes predeterminados para que cuando arranque el progreama el concesionario ya cuente con clientes
+     * */
     public void declarateClients() {
 
         clientes.add(new Cliente("12345678A", "Kevin", "+34 1234567"));
@@ -45,8 +51,9 @@ public class Concesionario {
     public List<Ventas> ventas = new ArrayList<>();
 
 
-    //Programa
-
+    /**
+     * Este es el run del programa
+     * */
     public void run() {
 
         while (true) {
@@ -90,9 +97,64 @@ public class Concesionario {
     }
 
 
+    /**
+     * Añade un cocehe al concesionario (arraylist de coches) preguntandole al usuario por cada uno de los datos del coche que va a añadir*/
     public void addCar() {
-        coches.add(new Coche(vista.askBrand(), vista.askmodel(), vista.askYear(), vista.askPrice(), vista.askNumberPlate(), false, vista.askKm()));
 
+        String brand = vista.askBrand();
+        String model = vista.askmodel();
+        int year;
+
+        //Compruebo que cuando el usuario ponga el año del coche este no sea menor a 1900 o mayor a 2026
+        while (true) {
+
+            year = vista.askYear();
+
+            if(year >= 1900 & year <= 2026) {
+
+                break;
+            }else {
+                vista.wrongImput();
+            }
+        }
+
+        double price;
+
+        //Compruebo que cuando el usuario ponga el precio este no sea menor que 30000 (ya no es un precio demaciado bajo para un coche, y este es un concesoinario de lujo), y que no sea mayor a 30millones (ya que no hay coches de lujo que cuesten mas que eso)
+
+        while (true) {
+
+            price = vista.askPrice();
+
+            if(price >= 30000 & price <= 30000000) {
+
+                break;
+            }else {
+                vista.wrongImput();
+            }
+        }
+
+        String numberPlate = vista.askNumberPlate();
+
+        double km;
+
+        //Compruebo que cuando el usuario ponga los km del coche estos no exedan los 60000 km ya que es demaciado uso para un coche que voy a vender en un concesionario de lujo
+
+        while (true) {
+
+            km = vista.askKm();
+
+            if(km >= 0 & km <= 60000) {
+
+                break;
+            }else{
+                vista.wrongImput();
+            }
+        }
+
+        coches.add(new Coche(brand, model, year, price, numberPlate, false, km));
+
+        //Compruebo los precios de todos los coches para saber cual es el mas alto y establecerlo como tope a la hora de buscar coches por precio
         for (Coche c : coches) {
             if(c.getPrice() > this.topPrice) {
                 this.topPrice = c.getPrice();
@@ -100,7 +162,11 @@ public class Concesionario {
         }
     }
 
-
+    /**
+     * Muestra todos los coches disponibles para vender en el concesionario (los que tienen el atributo sold = false)
+     *
+     * @return Lista de coches no vendidos
+     *     */
     public ArrayList<Coche> showCars() {
         List<Coche> cochesNoVendidos = new ArrayList<>();
         for (Coche c : coches) {
@@ -119,7 +185,8 @@ public class Concesionario {
 
 
 
-    //Buscar coches
+    /**
+     * Busca los coches por marca, año, o preco*/
     public void searchCars() {
 
         boolean found = false;
@@ -150,13 +217,23 @@ public class Concesionario {
 
             //Por Precio
             if (userIput == 2) {
+                int[] rango;
+                int min;
+                int max;
+                boolean underPrice = true; //Esta variable es para ver si e
+                // l precio maximo que ingreso el usuario es menor o mayor que el precio de mi coche mas caro
 
-                int[] rango = vista.searchCarsPrice();
+                while (true) {
+                    rango = vista.searchCarsPrice();
+                     min = rango[0];
+                     max = rango[1];
 
-                int min = rango[0];
-                int max = rango[1];
-
-                boolean underPrice = true;
+                    if (min > 0){
+                        break;
+                    }else{
+                        vista.wrongImput();
+                    }
+                }
 
                 if (max > this.topPrice) {
                     underPrice = false;
@@ -197,10 +274,10 @@ public class Concesionario {
                     vista.showCars(cochesEncontrados);
                 }
 
-                if (!found && (year > 2000 && year < 2026)) {
+                if (!found && (year > 1900 && year < 2026)) {
                     vista.yearNotFound();
                 }
-                if (!found && (year < 2000 || year > 2026)) {
+                if (!found && (year < 1900 || year > 2026)) {
                     vista.improbableYear();
                 }
             }
@@ -213,47 +290,39 @@ public class Concesionario {
 
     }
 
-
-    public boolean addClient() {
+    /**
+     * Agrega un cliente al arraylist de clientes
+     * @return Cliente existente o cliente nuevo
+     */
+    public Cliente addClient() {
         boolean exists = false;
         String dni = "";
+        Cliente client = null;
         dni = vista.addClientDni();
 
         for (Cliente c : clientes) {
             if (c.getDni().equals(dni)) {
-                vista.existentClientDni();
+                client = c;
+                vista.existedClient();
                 exists = true;
             }
         }
         if(!exists) {
+            vista.askToLogin();
             clientes.add(new Cliente(dni, vista.addClientName(), vista.addClientPhone()));
+            client = clientes.getLast();
         }
-        return exists;
+        return client;
     }
 
 
-    //Vender Coche
+    /**
+     * Vender los coches agregando las instancias del coche que se vendió y del cliente que lo compro, tambien el id de la venta y la fecha*/
     public void manageSale() {
 
-        //Pedir el dni del cliente para ver si esta guardado o es un nuevo cliente
-        boolean ourClient = false;
-        Cliente cliente = null;
+        vista.askBeforSale();
 
-        String dni = vista.askForDni();
-
-        for (Cliente c : clientes) {
-            if (c.getDni().equals(dni)) {
-                vista.existedClient();
-                cliente = c;
-                ourClient = true;
-            }
-        }
-        if (!ourClient) {
-            vista.askToLogin();
-            cliente = new Cliente(dni, vista.addClientName(), vista.addClientPhone());
-
-            clientes.add(cliente);
-        }
+        Cliente cliente = addClient();
 
         //Revisar que coches no se han vendido y mostrarlos
 
@@ -275,21 +344,22 @@ public class Concesionario {
             }
 
             //Guardar las ventas en el arraylist de ventas
-            coches.get(choise - 1).setVendido(true);
+            coches.get(choise - 1).setVendido(true);  //Ya que el coche se vendió convierto su atributo de vendido en true
             Coche coche = coches.get(choise - 1);
 
-            int id = ventas.size() +1;
+            int id = ventas.size() +1;  //Ya que voy a agregar una nueva venta el numero de esa venta sera igual a la cantidad de ventas que se se habian realizado + 1
 
             ventas.add(new Ventas(coche, cliente, id));
 
             vista.thanksForBuying();
 
         } else {
-            vista.carsSoldOut();
+            vista.carsSoldOut();  //En caso de que no queden coches para vender es muestra un mensaje de coches agotados
         }
     }
 
-    //Mostrar Ventas
+    /**
+     * Llama a la funcion de la vista que mustra el arraylist de ventas*/
     public void showSales() {
 
         vista.showSales(ventas);
--- Consecionario/src/Vista/Vista.java
+++ Consecionario/src/Vista/Vista.java
@@ -16,27 +16,31 @@ public class Vista {
 
     static Scanner sc = new Scanner(System.in);
 
+    /**
+     * Muestra el menu
+     *
+     * @return La opcion que eligió el usuario*/
     public int menu() {
         System.out.println(Blue + " " + "=========================" + RESET);
         System.out.println(Blue + "||" + RESET + "Bienvenisdo a UniqueCars" + Blue + "||" + RESET);
         System.out.println(Blue + " " + "=========================" + RESET);
         System.out.println("");
         System.out.println("Sleccione que quiere realizar:");
-        System.out.println(Blue + "------------------------------------" + RESET);
+        System.out.println(Blue + "+" + "----------------------------------" + "+" + RESET);
         System.out.println(Blue + "|" + RESET + " " + "1. Añadir coches al concesionario" + Blue + "|" + RESET);
-        System.out.println(Blue + "------------------------------------" + RESET);
+        System.out.println(Blue + "|" + "----------------------------------" + "|" + RESET);
         System.out.println(Blue + "|" + RESET + " " + "2. Mostar los coches disponibles " + Blue + "|" + RESET);
-        System.out.println(Blue + "------------------------------------" + RESET);
+        System.out.println(Blue + "|" + "----------------------------------" + "|" + RESET);
         System.out.println(Blue + "|" + RESET + " " + "3. Buscar coches disponibles     " + Blue + "|" + RESET);
-        System.out.println(Blue + "------------------------------------" + RESET);
+        System.out.println(Blue + "|" + "----------------------------------" + "|" + RESET);
         System.out.println(Blue + "|" + RESET + " " + "4. Registrar un nuevo cliente    " + Blue + "|" + RESET);
-        System.out.println(Blue + "------------------------------------" + RESET);
+        System.out.println(Blue + "|" + "----------------------------------" + "|" + RESET);
         System.out.println(Blue + "|" + RESET + " " + "5. Registrar una venta           " + Blue + "|" + RESET);
-        System.out.println(Blue + "------------------------------------" + RESET);
+        System.out.println(Blue + "|" + "----------------------------------" + "|" + RESET);
         System.out.println(Blue + "|" + RESET + " " + "6. Listar ventas                 " + Blue + "|" + RESET);
-        System.out.println(Blue + "------------------------------------" + RESET);
+        System.out.println(Blue + "|" + "----------------------------------" + "|" + RESET);
         System.out.println(Blue + "|" + RESET + " " + "7. Salir                         " + Blue + "|" + RESET);
-        System.out.println(Blue + "------------------------------------" + RESET);
+        System.out.println(Blue + "+" + "----------------------------------" + "+" + RESET);
 
 
         int userImput = sc.nextInt();
@@ -48,13 +52,16 @@ public class Vista {
 
 
 
-    //Valor no admitido
+    /**
+     * Muestra un mensaje de valor no admitido para cuando el usuario ponga algun valor que no deba*/
     public void wrongImput(){
         System.err.println("Valor no admitido");
     }
 
 
-    //Coches agotados
+
+    /**
+     * Muestra el mensaje de coches agotados cuando no quedan coches con el atributo sold = false*/
     public void carsSoldOut(){
         System.out.println("Lo sentimos, no tenemos coches disponibles");
     }
@@ -63,18 +70,27 @@ public class Vista {
 
 
     //Agregar Coche
+    /**
+     * Pide la marca del coche que se quiere agragar
+     * @return marca*/
     public String askBrand() {
 
         System.out.println("Que marca es el coche");
         String brand = sc.nextLine();
         return brand;
     }
+    /**
+     * Pide el modelo del coche que se quiere agragar
+     * @return moelo*/
     public String askmodel() {
 
         System.out.println("Que modelo es el coche");
         String model = sc.nextLine();
         return model;
     }
+    /**
+     * Pide el año del coche que se quiere agragar
+     * @return año*/
     public int askYear() {
 
         System.out.println("De que año es el coche");
@@ -82,6 +98,9 @@ public class Vista {
         sc.nextLine();
         return year;
     }
+    /**
+     * Pide el precio del coche que se quiere agragar
+     * @return precio*/
     public double askPrice() {
 
         System.out.println("Que precio tiene");
@@ -89,12 +108,18 @@ public class Vista {
         sc.nextLine();
         return prize;
     }
+    /**
+     * Pide la matricula del coche que se quiere agragar
+     * @return matricula*/
     public String askNumberPlate() {
 
         System.out.println("Dime numero de la matricula");
         String NumberPlate = sc.nextLine();
         return NumberPlate;
     }
+    /**
+     * Pide los kilometros recorridos por coche que se quiere agragar
+     * @return kilometros*/
     public double askKm() {
         System.out.println("Cuantos Kilometros tiene");
         double km = sc.nextDouble();
@@ -105,7 +130,10 @@ public class Vista {
 
 
 
-    //Mostrar lista de coches
+    /**
+     * Muestra la lista de coches
+     *
+     * @param coches de coches*/
     public void showCars(List<Coche> coches) {
         for(int i = 0; i < coches.size(); i++) {
 
@@ -118,23 +146,31 @@ public class Vista {
 
 
     //Buscar Coches
+    /**
+     * Muestra un menu secundario cuando el usuario elige la opcion de buscar coches, para que introduzca de que manera lo quiere buscar
+     *
+     * @return eleccion del usuario*/
     public int searchCars(){
         System.out.println("Elige como quieres buscar el coche");
         System.out.println("------------------------------------");
         System.out.println("1. Por la marca");
         System.out.println("2. Por el precio");
         System.out.println("3. Por el año");
-        System.out.println("4. Salir");
+        System.out.println("4. Regresar");
 
         int userImput = sc.nextInt();
         sc.nextLine();
         return userImput;
     }
-    //Por marca
+    /**
+     * Muestra un mensaje de que no se encontraron coches*/
     public void carNotFoundBrand(String brand) {
         System.out.println("Lo siento, no tenemos coches de la marca " + brand + " actualmente");
     }
-    //Por precio
+    /**
+     *Una vez que el usuario ha elegido buscar el coche por precio se le pide que ingrese el valor minimo y el valor maximo que esta dispuesto a pagar
+     *
+     *@return valor minimo y valor maximo en un array */
     public int[] searchCarsPrice() {
         System.out.println("Dame un rango de precios:");
         System.out.println("-------------------------");
@@ -151,14 +187,20 @@ public class Vista {
 
         return range;
     }
+    /**
+     * Muestra un mensaje de que no pudo encontrar un coche tan barato*/
     public void carNotFoundUnderPrice() {
-
         System.err.println("Lo siento, no tenemos un coche tan barato como ese");
     }
+    /**
+     * Muestra un mensaje de que no pudo encontrar un coche tan caro*/
     public void carNotFoundOverPrice(){
         System.err.println("Lo siento, no tenemos ningun coche tan caro como ese");
     }
-    //por año
+    /**
+     * Muestra el mensaje para saber de que año es el coche que el usuario esta buscando
+     *
+     * @return año*/
     public int searchCarsYear() {
 
         System.out.println("De que año estas buscando tu coche");
@@ -167,9 +209,14 @@ public class Vista {
         sc.nextLine();
        return year;
     }
+    /**
+     * Muestra el mensaje para cuando no se encuentra un coche que corresponda con el año buscado
+     */
     public void yearNotFound() {
         System.err.println("Lo siento, no tenemos coches de ese año");
     }
+    /**
+     * Muestra un mensaje para cuando el usuario introduce un año imposible*/
     public void improbableYear() {
         System.err.println("Año incorrecto");
     }
@@ -177,54 +224,72 @@ public class Vista {
 
 
 
+
+
     //Clientes
+    /**
+     * Muestra un mensaje pidiendo el dni del cliente
+     *
+     * @return dni del cliente*/
     public String addClientDni() {
         System.out.println("Introduce el dni del cliente");
         String dni = sc.nextLine();
         return dni;
     }
+    /**
+     * Muestra un mensaje pidiendo el nombre del cliente
+     *
+     * @return nombre del cliente*/
     public String addClientName(){
         System.out.println("Introduce el nombre ");
         String name = sc.nextLine();
         return name;
     }
+    /**
+     * Muestra un mensaje pidiendo el telefono del cliente
+     *
+     * @return telefono del cliente*/
     public String addClientPhone(){
         System.out.println("Introduce el telefono ");
         String phone = sc.nextLine();
         return phone;
     }
-    public void existentClientDni(){
-        System.err.println("Ese cliente ya existe");
-    }
-
-
-
-
-    //Vender Coche
-    public String askForDni(){
-        System.out.println("Antes de mostrarle los coches necesito el dni del cliente para identificarlo");
-        System.out.println("-----------------------------------------------------------------");
-        System.out.println("Introduzcalo por favor");
-        String dni = sc.nextLine();
-        return dni;
-    }
+    /**
+     * Muestra un mensaje de cliente existente*/
     public void existedClient(){
         System.out.println("Veo que ya es cliente nuestro");
         System.out.println("------------------------------");
     }
+    /**
+     * Muestra un mensaje para cuando detecte que es un cliente nuevo*/
     public void askToLogin(){
         System.out.println("Veo que es un nuevo cliente");
         System.out.println("----------------------------");
         System.out.println("Por favor introduzca sus datos:");
         System.out.println("-------------------------------");
     }
+
+
+
+
+
+    //Vender Coche
+    /**
+     * Muestra el mensaje que le pongo al usuario cuadno quiere realizar una compra*/
+    public void askBeforSale(){
+        System.out.println("Antes de mostrarle los coches necesito el dni del cliente para identificarlo");
+        System.out.println("-----------------------------------------------------------------");
+    }
+    /**
+     * Mensaje que se muestra antes de mostrar los coches disponibles*/
     public void messageToBuy(){
         System.out.println("A continuacion te muestro todos los coches que tenemos disponibles:");
         System.out.println("-------------------------------------------------------------------");
     }
-    public void showCarsWithOrder(Coche c, int contador){
-        System.out.println(contador + ": " + c);
-    }
+    /**
+     * Le pido al usuario que elija el coche que quiere comprar
+     *
+     * @return opcion del usuario*/
     public int askToPickCar(){
         System.out.println("----------------------------------------------");
         System.out.println("Introduce el numero del coche que te interesa");
@@ -233,6 +298,8 @@ public class Vista {
         sc.nextLine();
         return userImput;
     }
+    /**
+     * Mensaje de agradecimiento que se le muestra al usuario una vez realizada una venta*/
     public void thanksForBuying(){
         System.out.println("==================================================================");
         System.out.println("Muchas gracias por comprar nuestros coches. Que tenga un buen dia");
@@ -243,6 +310,10 @@ public class Vista {
 
 
     //Mostrar Ventas
+    /**
+     * Muestra todas las ventas registradas
+     *
+     * @param ventas*/
     public void showSales(List<Ventas> ventas){
 
         for (Ventas v : ventas) {
